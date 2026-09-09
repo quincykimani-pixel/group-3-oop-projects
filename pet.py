7690d06c05a91c2810783952f7326ccabdfc4c4d
MW4NG1 <msage3803@gmail.com>
2026-09-09 11:27:56 +0300

    add play method to Pet class to manage hunger and energy levels

--- pet.py
+++ pet.py
@@ -11,3 +11,9 @@ class Pet:
         self.hunger -= 1
         print(f"{self.name} enjoyed the food!")
         print(f"Hunger level: {self.hunger}")
+
+    def play(self):
+        """Increase hunger by 1 and decrease energy by 1 when playing."""
+        self.hunger += 1
+        self.energy -= 1
+        print(f"{self.name} is playing!")
